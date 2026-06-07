9f7eb3de363dcb5a995d8b2950ee37b169f087bf
Kead <flostarkead5153@gmail.com>
2026-06-08 01:40:46 +0200

    Refactor project into CLI structure

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+mkdir -p bin
+
+g++ src/yt_media.cpp -o bin/yt_media
+
+echo "Build complete!"
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+./bin/yt_media
